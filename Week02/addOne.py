f57f878915d9505be7d8ca88a85b1ea238fbf7ad
Carolyn Moorhouse <G00398299@gmit.ie>
2021-02-15 20:07:54 +0000

    Week02

--- Week02/addOne.py
+++ Week02/addOne.py
@@ -2,6 +2,8 @@
 # this program reads a number and add one
 # author: Carolyn Moorhouse
 
+# we must convert the string so input returns the int
+
 number = int(input ('Please enter your number:'))
 NewNumber = number + 1
 print ('{} plus one is {}'.format(number, NewNumber))
