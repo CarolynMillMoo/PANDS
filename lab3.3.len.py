3a1bb4827c528203dde5ed4b93245dc0c32f84b8
CarolynMillMoo <77897923+CarolynMillMoo@users.noreply.github.com>
2021-03-09 21:59:08 +0000

    Add files via upload

---
+++ lab3.3.len.py
@@ -0,0 +1,7 @@
+#this program reads in a string and outputs its length
+#Author: Carolyn Moorhouse
+
+inputString = input('Enter a string:')
+lengthOfString = len(inputString)
+print('The length of {} is {}'.format(inputString, lengthOfString))
+
