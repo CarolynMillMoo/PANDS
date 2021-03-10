8a005a35e43cb55a2ec06d8ddf991237d6cad756
Carolyn Moorhouse <G00398299@gmit.ie>
2021-03-10 19:09:20 +0000

    This updated version has just been altered so if the student got a 39.5, 49.5, 59.5, 69.5 their grade would be updated to the next band

--- Week04-flow/lab4.1.grade.py
+++ Week04-flow/lab4.1.grade.py
@@ -4,18 +4,17 @@
 #Author: Carolyn Moorhouse
 
 percentage = float(input("Enter the percentage: "))
-round(percentage)
 
 if percentage < 0 or percentage > 100:
     print("Please enter a number between 0 and 100")
 
-elif percentage < 40:
+elif percentage < 39.5:
     print ("Fail")
-elif percentage < 50:
+elif percentage < 49.5:
     print ("Pass")
-elif percentage < 60:
+elif percentage < 59.5:
     print ("Merit1")
-elif percentage < 70:
+elif percentage < 69.5:
     print ("Merit2")
 else: #the only valid percentage remaining is between 70 and 100
     print ("Distinction")
