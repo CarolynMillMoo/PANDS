dadbcecfc3e295d8ebb7e54c5444a62b290e0afb
Carolyn Moorhouse <G00398299@gmit.ie>
2021-04-02 13:49:13 +0100

    this is much quicker

--- Topic05-datastructures/primes.py
+++ Topic05-datastructures/primes.py
@@ -7,9 +7,12 @@ upto = 100000
 for candidate in range(2, upto):
   #print(candidate)
   isPrime = True
-  for divisor in range(2, candidate):
+  #only need to check if divisble by prime
+  for divisor in primes:
+      #if divisable by an int it is not a prime number
       if (candidate % divisor == 0):
           isPrime = False
+          #so there is no reason to keep checking
           break
 
   if isPrime:       
