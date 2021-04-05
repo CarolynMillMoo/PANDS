d5cd55e48b2af1dbd4bd89ec73554c564b15577c
Carolyn Moorhouse <G00398299@gmit.ie>
2021-04-05 17:30:56 +0100

    adding in work completed on lecture 6 and lab06 thus far

--- Week06-functions/primes.py
+++ Week06-functions/primes.py
@@ -7,12 +7,10 @@ upto = 1000
 
 def isPrime(candidate):
     #check if it is a prime
-    numisPrime = True
-    for divisor in range(2, math.floor(math.sqrt(candidate))):
+       for divisor in range(2, math.floor(math.sqrt(candidate))):
         if candidate % divisor == 0:
-            numisPrime = False
-            break
-    return numisPrime
+            return False
+       return True
 
 
 candidates = range(2, upto+1)
---
+++ Week06-functions/quiz.py
@@ -0,0 +1,11 @@
+#quiz week06 lab
+#Author: Carolyn Moorhouse
+
+#function yo takes one parameter
+def yo(a):
+    return a*2
+#the function is called yo
+#and we are passing in an argument, 3
+print (yo(3))
+print ("Answer to a. nothing")
+print ("Answer to b. add in print(yo(3))")
