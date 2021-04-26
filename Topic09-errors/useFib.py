1b508d62cca61029176f55140024fe08ce2dc9e0
Carolyn Moorhouse <G00398299@gmit.ie>
2021-04-26 21:20:51 +0100

    Lab09

---
+++ Topic09-errors/useFib.py
@@ -0,0 +1,8 @@
+#This program prompts the user for a number and 
+#prints out the fibonacci sequence of that many numbers
+#Author: Carolyn Moorhouse
+
+import myFunctions
+
+nTimes = int(input('How many:'))
+print(myFunctions.fibonacci(nTimes))
